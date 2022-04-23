bb7216c4fd57848f148d82c0abf8ff2f2a34a4c8
Auliarohmatulhasanah <92772975+Auliarohmatulhasanah@users.noreply.github.com>
2022-04-22 22:42:35 -1100

    Add files via upload

---
+++ Project 1 Mandiri/Praktikum Pekan Ke 3/Startboostrap/View/artikel1.php
@@ -0,0 +1,129 @@
+<?php
+include '../layout/header.php';
+?>
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-6">
+			<div class="jumbotron">
+				<h2>
+					Welcome to Artikel
+				</h2>
+				<p>
+                Butterflies and moths (rama-rama) are insects belonging to the order Lepidoptera, or 'scale-winged insects' (lepis, scales and pteron, wings).
+				</p>
+				<p>
+					<a class="btn btn-primary btn-large" href="#">Learn more</a>
+				</p>
+			</div>
+			<div class="row">
+				<div class="col-md-4">
+					<div class="card">
+                    <img src="../assets/img/Egg.jpg" class="img-thumbnail" alt="">
+						<div class="card-block">
+							<h5 class="card-title">
+								Egg butterfly
+							</h5>
+							<p class="card-text">
+                            Butterfly eggs are protected by a tough burrowing shell called the chorion covered with an anti-wax coating that protects the eggs from drying out before the larvae can fully develop. Each egg has a fine funnel-shaped pore at one end, the micropyle which aims to allow the entry of sperm to join the egg. Different species have different egg sizes, but all butterfly eggs are spherical or oval in shape.
+
+                            Butterfly eggs are attached to the leaves with a special adhesive that hardens quickly. When it hardens, it contracts and bends the shape of the egg. This adhesive is easily seen to form the meniscus material that surrounds the site of each egg. This adhesive is also produced by the pupa to bind the cremaster seta. This adhesive is so hard that the silk lining that holds the seta together cannot be separated.
+
+                            Butterfly eggs are always laid on plants. Each species of butterfly has its own range of host plants, both single species and multiple species. The egg stage is passed over several weeks for most butterflies, but eggs that are released shortly before winter, especially in temperate climates, must go through a diapaus (resting) stage and only hatch in the spring. There are other butterfly species that lay eggs in the spring so that the eggs can hatch in the summer.
+							</p>
+							<p>
+								<a class="btn btn-primary" href="#">Action</a>
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card">
+                    <img src="../assets/img/Larva.jfif" class="img-thumbnail" alt="">
+						<div class="card-block">
+							<h5 class="card-title">
+                            Caterpillar butterfly
+							</h5>
+							<p class="card-text">
+                            Butterfly larvae, namely caterpillars, feed on plant leaves and spend all their time as shells looking for food. Most beluncas are mauns, but there are species such as Spalgis epius and Liphyra brassolis that feed on insects.
+
+                            Some larvae, especially those belonging to the Lycaenidae, establish mutually beneficial relationships with ants. Beluncas communicate with ants by using vibrations emitted through the substrate in addition to emitting chemical signals. The ants protect these larvae more or less; in return, the larvae help the ants collect the honey seepage.
+
+                            Beluncas enlarge through a series of stages called instars. Towards the end of each instar, the larvae undergo a process called apolysis, in which the epidermis, the tough outer layer made of a mixture of chitin and special proteins, is expelled from the soft underlying epidermis, the epidermis forming the new epidermis below. At the end of each instar, the larva gives birth to its old skin, then the new skin develops and then hardens and produces pigment rapidly.The process of copying this skin can take days. The butterfly flap pattern begins to develop on the body of the beluncas towards the last instar.
+							</p>
+							<p>
+								<a class="btn btn-primary" href="#">Action</a>
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card">
+                    <img src="../assets/img/Butterfly.jpg" class="img-thumbnail" alt="">
+						<div class="card-block">
+							<h5 class="card-title">
+								Butterfly
+							</h5>
+							<p class="card-text">
+                            Butterflies are generally active during the day (diurnal), while moths are mostly active at night (nocturnal). Butterflies rest or perch by straightening their wings, moths perch by spreading their wings. Butterflies usually have beautiful brilliant colors, moths tend to be dark, dull or gray. However, there are always exceptions to these differences, so they cannot scientifically be used as a definitive guide. (Van Mastrigt and Rosariyanto, 2005).
+
+                            There are many kinds of butterflies and moths, and on the islands of Java and Bali alone, more than 600 species of butterflies have been recorded. So far, no complete list of moth species has been made, but hundreds of species are suspected (Whitten et al., 1999). Butterflies are one of the few types of insects that are harmless to humans.
+							<p>
+								<a class="btn btn-primary" href="#">Action</a>
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-6">
+			<dl>
+				<dt>
+                Habits and food
+				</dt>
+				<dd>
+                Many people like beautiful butterflies, but on the contrary, it is rare for people who do not feel disgusted with caterpillars, even though they are the same creature. All types of butterflies and moths go through life stages as eggs, caterpillars, pupae, and finally metamorphose into butterflies or moths.
+
+                Butterflies generally live by sucking flower nectar (nectar). However, some species like the liquid sucked from fruit that falls on the ground and rots, carrion meat, bird droppings, and wet soil.
+
+                Unlike butterflies, caterpillars live mainly by eating leaves. These caterpillars are very voracious, but generally each type of caterpillar specializes in eating the leaves of certain types of plants. So that the presence of a type of butterfly in a place is also determined by the availability of plants that host the caterpillars.
+				</dd>
+				<dt>
+                Butterflies and humans
+				</dt>
+				<dd>
+                Butterflies and moths are known as pollinating insects, which helps flowers develop into fruit. So for farmers, and people in general, these butterflies are very useful to help pollinate plants.
+
+                On the other hand, various types of caterpillars are known as voracious pests. It is not only seasonal crops that are preyed upon, but also fruit trees and trees in general can be defoliated by caterpillars in a relatively short time. Many types of caterpillar pests, especially from the types of moths are serious agricultural pests.
+
+                To take advantage of the beauty of several types, people are now developing butterfly farms.
+				</dd>
+				<dt>
+                Life cycle
+				</dt>
+				<dd>
+                Many believe that butterflies have a very short lifespan. In fact, an adult butterfly can live for a week or almost a year depending on the species. Most species go through a rather long larval stage, and some are able to become dormant when in the pupa or egg stage in order to be able to wade through the winter.
+
+                Butterflies can lay eggs once or many times each year. The number of offspring per year differs depending on the climate, where butterflies living in the tropics are able to lay eggs more than once a year.
+				</dd>
+				<dt>
+                The role and benefits of butterflies
+				</dt>
+				<dd>
+                The role of the butterfly on the environment is as: Environmental Bioindicator, Pollinator, and Inspiration for Technological Innovation. Butterflies with bright colors indicate that the environment is beautiful, while butterflies with dull or dark colors indicate that the environment is polluted. The dull color is a form of adaptation made by the butterfly to its environment. Butterflies are animals that help pollinate plants which are called pollinators. Butterflies are the inspiration for technological innovation, namely by inspiring textile entrepreneurs to make elegant textile materials and the stacked scale arrangement is the inspiration for the creation of tools to absorb solar energy.
+
+                Butterflies are not only concerned with beauty or their habitat and way of life, but the problem of overexploitation that is not accompanied by maintenance or conservation causes problems of scarcity to extinction. For example, Ornithoptera paradisea (Butterfly of Paradise) which is found in Papua (Indonesia and Papua New Guinea).
+				</dd>
+			</dl>
+			<p class="text-center text-success">
+            Butterflies Symbolize Beautiful Feelings of Love
+
+            Butterflies have a very beautiful face with beautiful colors on the wings. The beauty of the butterfly is often a symbol of love. The philosophy of this beautiful butterfly teaches that love does look beautiful.
+			</p>
+		</div>
+	</div>
+</div>
+
+<?php
+include '../layout/footer.php';
+?>
---
+++ Project 1 Mandiri/Sidebar.php
@@ -0,0 +1,129 @@
+ <!-- Main Sidebar Container -->
+ <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="../../index3.html" class="brand-link">
+      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">AdminLTE 3</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">Alexander Pierce</a>
+        </div>
+      </div>
+
+      <!-- SidebarSearch Form -->
+      <div class="form-inline">
+        <div class="input-group" data-widget="sidebar-search">
+          <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+          <div class="input-group-append">
+            <button class="btn btn-sidebar">
+              <i class="fas fa-search fa-fw"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="Praktikum Pekan Ke 1/array_data_table.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Praktikum 1</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Praktikum 2</p>
+                </a>
+                <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                    <a href="Praktikum Pekan Ke 2/Form_Belanja_Online.php" class="nav-link">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>Form Belanja</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="Praktikum Pekan Ke 2/Form_Nilai_Siswa.php" class="nav-link">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>Form Nilai Siswa</p>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a href="Praktikum Pekan Ke 3/Startboostrap/" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Praktikum 3</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Praktikum 4</p>
+                </a>
+                <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                    <a href="Praktikum pekan ke 4/Tugas 2/Class_Mahasiswa.php" class="nav-link">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>Mahasiswa</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="Praktikum Pekan Ke 4/Latihan/Form.php" class="nav-link">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>Data siswa</p>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a href="Praktikum Pekan Ke 5/class_accountbank.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Praktikum 5</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item menu">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-copy"></i>
+              <p>
+                Layout Options
+                <i class="fas fa-angle-left right"></i>
+               
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="Pasien.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>BMI</p>
+                </a>
+              </li>
+              
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
